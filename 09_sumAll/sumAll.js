003fb8a8176bc972370fbffee5a4b54550579d45
Boofa Legion Linux <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-18 11:04:41 +1000

    complete exercise 09
    Commented out 2 lines of attempted solution and replaced with single
    line from solution file. Left commented attempt for reference.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    // if (min % 1 !== 0 || max % 1 !== 0) return "ERROR";
+    // if (typeof min !== "number" || typeof max !== "number") return "ERROR";
+    let sum = 0;
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
