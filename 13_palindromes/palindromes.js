8c97803eacd422c889b8f20c9f4a4850fb0a8421
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-24 13:03:11 +1000

    complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    let formatted = phrase
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumeric.includes(char))
+        .join("");
+
+    let reversed = formatted.split("").reverse().join("");
+
+    return formatted === reversed;
 };
 
 // Do not edit below this line
