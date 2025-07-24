14a3348f65eaed16183b5e52ea298e79cdfa5cf5
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-24 13:52:39 +1000

    complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr
+        .map((person) => {
+            if (person.yearOfDeath === undefined) {
+                person.yearOfDeath = new Date().getFullYear();
+            }
+            return person;
+        })
+        .sort((a, b) => {
+            return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+        })[0];
 };
 
 // Do not edit below this line
