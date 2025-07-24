afa8df704605bdf2641fba7a0cdefc326df09f9e
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-24 13:29:03 +1000

    complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let index = parseInt(num);
 
+    if (index === 0) {
+        return 0;
+    } else if (index < 0) {
+        return "OOPS";
+    }
+
+    const sequence = [1, 1];
+    for (let i = 2; i < num; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence[num - 1];
 };
 
 // Do not edit below this line
