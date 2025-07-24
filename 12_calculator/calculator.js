96107e5fd69963af33ce95c4c48ef4c828ef2878
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-24 11:27:21 +1000

    complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+    return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let sum = 1;
+    for (let i = a; i > 0; i--) {
+        sum *= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
